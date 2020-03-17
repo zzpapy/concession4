5faeafb0b76a49d8d2d1b449712839206579c875
zzpapy <gregory.pace@hotmail.fr>
2020-03-17 13:22:32 +0100

    premier commit

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -10,7 +10,7 @@
             $man = new VehiculeManager();
 
             $vehicules = $man->findAll();
-            var_dump($vehicules);
+
             return $this->home([
                 "view" => "liste.php",
                 "data" => $vehicules
@@ -29,14 +29,6 @@
             ]);
         }
         public function crea(){
-            return $this->home([
-                "view" => "crea.php",
-                "data" => ""
-            ]);
-        }
-        public function traitementCrea($tab){
-            $man = new VehiculeManager();
-            $new = $man->add($tab);
             return $this->home([
                 "view" => "crea.php",
                 "data" => ""
--- index.php
+++ index.php
@@ -3,32 +3,27 @@
 
     use Controller\HomeController;
 
+    var_dump($_POST);
     define('DS', DIRECTORY_SEPARATOR); // le caractère séparateur de dossier (/ ou \)
     // meilleure portabilité sur les différents systêmes.
     define('BASE_DIR', dirname(__FILE__).DS); // pour se simplifier la vie
     define('VIEW_DIR', BASE_DIR."view/");     //le chemin où se trouvent les vues
-    
+
     require("app/Autoloader.php");
     Autoloader::register();
-    
+
     $ctrl = new HomeController();
-    var_dump($_GET);
+    // var_dump($_GET);die;
     if(isset($_GET['action'])){
         $action = $_GET['action'];
     }
-    if(isset($_GET['action']) && $_GET["action"]=="crea"){
-        if(!empty($_POST)){
-            $action = "traitementCrea";
-            var_dump(isset($_POST));
-        }
-    }
     else $action = "home";
-    
+
     if(isset($_GET['id'])){
         $id = $_GET['id'];
     }
     else $id = null;
-    
+
     $render = $ctrl->$action($id);
-    var_dump($render);
+// var_dump($render);
     require($render['view']);
